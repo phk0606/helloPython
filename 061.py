a77e9a1e0009bef9d50a000799d4a5c96c0d407d
hkpark <phk0606@naver.com>
2021-09-24 13:10:33 +0900

    파이썬 실습

---
+++ 061.py
@@ -0,0 +1,13 @@
+numdata = 57
+strdata = '파이썬'
+listdata = [1,2,3]
+dicdata = {'a':1, 'b':2}
+
+def func():
+    print('안녕하세요')
+
+print(type(numdata))
+print(type(strdata))
+print(type(listdata))
+print(type(dicdata))
+print(type(func))
